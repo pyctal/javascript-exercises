2509fbeb02271053e32690490834fd1f30e913fb
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-05 20:43:43 +0530

    Refactor solution for exercise 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
 const leapYears = function (year) {
-  return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+  const isYearDivisibleByFour = year % 4 === 0;
+  const isYearDivisibleByHundred = year % 100 === 0;
+  const isYearDivisibleByFourHundred = year % 400 == 0;
+
+  return (
+    isYearDivisibleByFour &&
+    (!isYearDivisibleByHundred || isYearDivisibleByFourHundred)
+  );
 };
 
 // Do not edit below this line
