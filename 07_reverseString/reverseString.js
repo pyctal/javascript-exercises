732afcc76a1c7b685b137fbc62d1824d2c19281a
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-05 16:53:44 +0530

    Complete exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let returnString = "";
+  for (let i = 0; i < str.length; i++) {
+    returnString = str[i] + returnString;
+  }
+  return returnString;
 };
 
 // Do not edit below this line
