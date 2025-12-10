9903cdcb7112ce37c7484ab7c079cd08e72a6c28
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-10 22:37:13 +0530

    Complete exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const cleanStr = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
+  console.log(cleanStr);
 
+  for (let index = 0; index < cleanStr.length / 2; index++) {
+    if (cleanStr.at(index) !== cleanStr.at(-1 - index)) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
