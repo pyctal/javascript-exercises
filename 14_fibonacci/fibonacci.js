d254e613eb07fe1255ee3df110777c6a91c266e3
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-10 23:11:37 +0530

    Complete exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = typeof n === "number" ? n : parseInt(n);
+  if (n < 0) {
+    return "OOPS";
+  } else if (n === 0) {
+    return 0;
+  }
 
+  let firstPrev = 1;
+  let secondPrev = 0;
+  for (let i = 2; i <= n; i++) {
+    [firstPrev, secondPrev] = [firstPrev + secondPrev, firstPrev];
+  }
+
+  return firstPrev;
 };
 
 // Do not edit below this line
