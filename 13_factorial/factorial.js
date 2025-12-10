a7e45b814929a7474d29ad70f74f6f278c6ba8be
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-10 22:03:31 +0530

    Complete exercise 13_factorial

--- 13_factorial/factorial.js
+++ 13_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (n) {
+  if (!Number.isInteger(n) || n < 0) {
+    return undefined;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
 
 // Do not edit below this line
