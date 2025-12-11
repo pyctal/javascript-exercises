74a2c10611d7135c698678787ca4a56eef8860e7
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-11 12:02:29 +0530

    Complete exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(person) {
+  if (person.yearOfDeath) {
+    return person.yearOfDeath - person.yearOfBirth;
+  }
+  return new Date().getFullYear() - person.yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(current);
+    return oldestAge > currentAge ? oldest : current;
+  });
 };
 
 // Do not edit below this line
